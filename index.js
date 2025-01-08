040e61e1618540e9c6eeddb2071dc90cf1c3f7d6
shubham kumar <clboy768@gmail.com>
2025-01-08 16:18:10 +0530

    updated

--- index.js
+++ index.js
@@ -15,10 +15,11 @@ let postRouter = require('./routes/postRoutes')
 let messageRouter = require('./routes/messageRoutes')
 
 app.use(cors({
-    origin:'https://mediaapp-backend-jodl.onrender.com',
-    credentials: true,
-    methods:['post','get','put','delete']
-}))
+    origin: 'https://mediaapp-backend-jodl.onrender.com',  
+    credentials: true, 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  }));
+  
 app.use(express.json({ limit: '100mb' }))
 
 app.set('view engine', 'ejs')
